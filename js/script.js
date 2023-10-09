e6da4645dc9394d03ca8b757d019fbbbfcec038f
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-09 15:08:27 +0200

    Variable and Conditions

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Variabili
+
+const cost = 0.21;
+let age = prompt('Inserisci gli anni');
+let age_2 = prompt('Inserisci la seconda età');
+let km = prompt('Inserisci i km da percorrere');
+
+console.log(age);
+console.log(age_2);
+console.log(km);
+
+//Condizioni
+
+if(isNaN(age) || isNaN(age_2) || isNaN(km)){
+    window.location.reload();
+}
+
+if(age < age_2){
+    
+}
+
+
+
+
